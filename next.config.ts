d39dd3278f2b1a4c3c00b550e19ed8a74015041f
Szymon Nowak <sn469481@students.mimuw.edu.pl>
2025-11-11 17:07:07 +0100

    Removed prefix after changing domain

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const isProd = (process.env.NODE_ENV || 'production') === 'production';
 const nextConfig: NextConfig = {
   output: 'export',                  // for static export
   images: { unoptimized: true },     // needed for next export
-  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
+  // basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
+  // assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
 };
-  
+
 export default nextConfig;
