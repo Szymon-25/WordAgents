7effae7786335d8e376dd67384f378a6f9ba613b
Szymon Nowak <sn469481@students.mimuw.edu.pl>
2025-11-11 14:52:14 +0100

    Fixed path for Github Pages

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,9 @@ const isProd = (process.env.NODE_ENV || 'production') === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',                  // for static export
-  images: { unoptimized: true },     // needed for next export
-  trailingSlash: true,               // GitHub Pages prefers trailing slashes
   basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
   assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
